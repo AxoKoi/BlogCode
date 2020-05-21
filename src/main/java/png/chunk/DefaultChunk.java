b7e6f5eb1b31d89fb75be47c883415f6f8eb2041
Ignacio Rondini <jirondini@gmail.com>
2020-05-21 12:55:12 +0200

    Deprecate DefaultChunk
    Default chunk should be removed after concrete chunk specification!

--- src/main/java/png/chunk/DefaultChunk.java
+++ src/main/java/png/chunk/DefaultChunk.java
@@ -2,6 +2,12 @@ package png.chunk;
 
 import java.nio.ByteBuffer;
 
+
+/**
+ * This class is a development class. It will be removed
+ * after the all the concrete chunks are implemented.
+ */
+@Deprecated
 public class DefaultChunk extends Chunk {
     DefaultChunk(ByteBuffer buffer) {
         super(buffer);
