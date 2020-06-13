8cc0240b137704ebf7ec2cdf9681c866437696da
Ignacio Rondini <jirondini@gmail.com>
2020-06-13 07:15:53 +0200

    Test modification
    Assert the CRC too

--- src/test/java/com/axokoi/png/chunk/ChunkFactoryTest.java
+++ src/test/java/com/axokoi/png/chunk/ChunkFactoryTest.java
@@ -4,7 +4,6 @@ For more information, visit www.axokoi.com or www.github.com/axokoi
 */
 package com.axokoi.png.chunk;
 
-import com.axokoi.png.chunk.*;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.Assert;
@@ -18,13 +17,15 @@ import static org.junit.Assert.assertTrue;
 public class ChunkFactoryTest {
 
     @Test
-    public void parse() throws DecoderException, IllegalChunkException {
+    public void parseIHDR() throws DecoderException, IllegalChunkException {
         byte[] rawChunk = Hex.decodeHex("0000000D4948445200000500000004C70806000000B27EDA4F");
         Chunk chunk = ChunkFactory.parse(ByteBuffer.wrap(rawChunk));
 
         assertEquals(AllowedChunkTypes.IHDR, chunk.getType());
         assertTrue(chunk instanceof IHDRChunk);
         Assert.assertArrayEquals(rawChunk, chunk.bytes());
+        byte[] expectedCRC = {(byte) 0xB2, 0x7E, (byte) 0xDA, 0x4F};
+        Assert.assertArrayEquals(expectedCRC, chunk.getCRC());
 
         IHDRChunk headerChunk = (IHDRChunk) chunk;
         assertEquals(0x500, headerChunk.getWidth());
@@ -43,4 +44,11 @@ public class ChunkFactoryTest {
         byte[] rawChunk = Hex.decodeHex("0000000D9999999900000500000004C70806000000B27EDA4F");
         ChunkFactory.parse(ByteBuffer.wrap(rawChunk));
     }
+    
+    @Test
+    public void parseTRNS(){
+        //todo
+        //todo
+    }
+
 }
