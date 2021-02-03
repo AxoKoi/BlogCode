7e94b8cbb93a6bc67d32fff928cc7c2161f1b979
Ignacio Rondini <ignacio.rondini@confidens.zetes.com>
2021-02-03 08:42:35 +0100

    Remove unused imports.

--- greetingApp/tomcat-greeting/src/main/java/com/axokoi/tomcatgreeting/TomcatGreetingApplication.java
+++ greetingApp/tomcat-greeting/src/main/java/com/axokoi/tomcatgreeting/TomcatGreetingApplication.java
@@ -8,18 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Map;
-
 @SpringBootApplication
 @RestController
 public class TomcatGreetingApplication {
 
-
    @GetMapping("/greeting/{name}")
    public ResponseEntity<String> hello(@PathVariable String name) {
       return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
-      //return Collections.singletonMap("text", "Hello " + name);
    }
 
    public static void main(String[] args) {
